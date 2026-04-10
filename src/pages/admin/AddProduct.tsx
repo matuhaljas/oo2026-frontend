ca4de78988e40c6a1d9a2905bfbda6d56336d39a
Marcus Haljasoks <144263566+matuhaljas@users.noreply.github.com>
2026-04-10 12:17:20 +0300

    pagination

--- src/pages/admin/AddProduct.tsx
+++ src/pages/admin/AddProduct.tsx
@@ -9,38 +9,39 @@ function AddProduct() {
     active: false,
     stock: 0,
     category: {
+      id: 1,
       name: ""
     }
   });
 
-  const AddProduct = () => {
-        fetch(import.meta.env.VITE_BACK_URL + "/products", {
-            method: "POST",
-            body: JSON.stringify(newProduct),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json())
-        .then(() => alert("Uus toode on lisatud." ));
-    }
+  const addProduct = () => {
+    fetch(import.meta.env.VITE_BACK_URL + "/products", {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(res => res.json())
+      .then(() => alert("Toode lisatud!"));
+  }
 
   return (
     <div>
       <label>Name</label> <br />
-      <input onChange={(e) => setNewProduct({...newProduct, name: e.target.value})} type="text"/> <br />
+      <input onChange={(e) => setNewProduct({...newProduct, name: e.target.value})} type="text" /> <br />
       <label>Description</label> <br />
-      <input onChange={(e) => setNewProduct({...newProduct, description: e.target.value})} type="text"/> <br />
+      <input onChange={(e) => setNewProduct({...newProduct, description: e.target.value})} type="text" /> <br />
       <label>Price</label> <br />
-      <input onChange={(e) => setNewProduct({...newProduct, price: Number(e.target.value)})} type="number"/> <br />
+      <input onChange={(e) => setNewProduct({...newProduct, price: Number(e.target.value)})} type="number" /> <br />
       <label>Active</label> <br />
-      <input onChange={(e) => setNewProduct({...newProduct, active: e.target.checked})} type="checkbox"/> <br />
+      <input onChange={(e) => setNewProduct({...newProduct, active: e.target.checked})} type="checkbox" /> <br />
       <label>Stock</label> <br />
-      <input onChange={(e) => setNewProduct({...newProduct, stock: Number(e.target.value)})} type="number"/> <br />
+      <input onChange={(e) => setNewProduct({...newProduct, stock: Number(e.target.value)})} type="number" /> <br />
       <label>Category</label> <br />
       {/* <select>
         {categories.map(category => <option></option>)}
       </select> */}
-      <button onClick={AddProduct}>Add Product</button>
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
