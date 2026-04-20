e226223d1c3823a2a2ccff144deafe9691244f80
Marcus Haljasoks <144263566+matuhaljas@users.noreply.github.com>
2026-04-20 20:25:59 +0300

    muutused

--- src/pages/admin/ManageProducts.tsx
+++ src/pages/admin/ManageProducts.tsx
@@ -2,42 +2,40 @@ import { useEffect, useState } from "react"
 import type { Product } from "../../models/Product";
 
 function ManageProducts() {
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    //uef --> enter
-    useEffect(() => {
-        fetch(import.meta.env.VITE_BACK_URL + "/products") // URL kuhu läheb päring
-            .then(res => res.json())
-            .then(json => setProducts(json))
-    }, []);
+  useEffect(() => {
+  fetch(import.meta.env.VITE_BACK_URL + "/products/admin")
+    .then(res => res.json())
+    .then(json => setProducts(json));
+  }, []);
 
-    const deleteProduct = (productId: number) => {
-      fetch(import.meta.env.VITE_BACK_URL + "/products/" + productId, {
-        method: "DELETE"
-      }).then(res => res.json())
+  const deleteProduct = (productId: number) => {
+    fetch(import.meta.env.VITE_BACK_URL + "/products/" + productId, {
+      method: "DELETE"
+    }).then(res => res.json())
       .then(json => setProducts(json));
-    }
-
+  }
 
   return (
     <div>
       <table>
         <thead>
           <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>description</th>
-            <th>price</th>
-            <th>active</th>
-            <th>stock</th>
-            <th>category</th>
-            <th>edit</th>
-            <th>delete</th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Active</th>
+            <th>Stock</th>
+            <th>Category</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {products.map(product => 
-            <tr key = {product.id}>
+            <tr key={product.id}>
               <td>{product.id}</td>
               <td>{product.name}</td>
               <td>{product.description}</td>
@@ -46,12 +44,12 @@ function ManageProducts() {
               <td>{product.stock}</td>
               <td>{product.category?.name}</td>
               <td><button>Edit</button></td>
-              <td><button onClick={() => deleteProduct(Number(product.id))}>X</button></td>
+              <td><button onClick={() => deleteProduct(Number(product.id))}>x</button></td>
             </tr>)}
         </tbody>
       </table>
     </div>
-    )
+  )
 }
 
 export default ManageProducts
